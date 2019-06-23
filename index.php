6165565442453a4e838374f17499f3a16445ff09
Vithuran Koneswarathsan <vkoneswarathsan@gmail.com>
2019-06-23 21:12:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "This is a test";
+$servername = "localhost";
+$username = "Vitman";
+$password = "Looser96";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
